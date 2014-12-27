bb804a37affd19ad35989428ce6d85140a3fe28d
juliocnsouza <juliocnsouzadev@gmail.com>
2014-12-27 07:08:38 -0200

    app settings

--- mean/public_html/app.js
+++ mean/public_html/app.js
@@ -7,3 +7,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views')); //caminho para camada views setado para pasta views
 
 app.set('view engine', 'ejs');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log("Servidor inicializado na porta " + server.aaddress().port);
+});
