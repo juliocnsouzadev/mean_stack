8a227ddbe490853f548a03e0c1f0739fee14bfa0
juliocnsouza <juliocnsouzadev@gmail.com>
2015-08-27 17:03:27 -0300

    atualizacao

--- MEAN_obrasArtes/public_html/testes2.js
+++ MEAN_obrasArtes/public_html/testes2.js
@@ -5,21 +5,21 @@ var json123 = '{"jardineiro":"Monalisa", "jesuis": {"pintura": "mais linda", "co
 
 var um = JSON.parse(json123);
 users.insert(um);
-//var dois = {
-//    cabra_macho: 24,
-//    milho_verde: {soboro: "mais linda", clear_arv: false},
-//    zoi_d_lula: "quem falou?",
-//    macueba: "2345678",
-//    arv_seca: true
-//}
-//users.insert(dois);
-//var tres = {
-//    nome: "Rafael",
-//    idade: 22,
-//    sexo: "?",
-//    casado_com: {nome: "Janine", idade: 25, gosto_musical: {tipo: "rock", lista: ["pretty reckless", "fiona apple"]}}
-//}
-//users.insert(tres);
+var dois = {
+    cabra_macho: 24,
+    milho_verde: {soboro: "mais linda", clear_arv: false},
+    zoi_d_lula: "quem falou?",
+    macueba: "2345678",
+    arv_seca: true
+}
+users.insert(dois);
+var tres = {
+    nome: "Rafael",
+    idade: 22,
+    sexo: "?",
+    casado_com: {nome: "Janine", idade: 25, gosto_musical: {tipo: "rock", lista: ["pretty reckless", "fiona apple"]}}
+}
+users.insert(tres);
 db.close();
 
 
